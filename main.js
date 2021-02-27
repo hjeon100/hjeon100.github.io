48cfd85862adcb51022d3d35ca1b23ccb5907437
Hoon Pyo Jeon <hjeon100@gmail.com>
2021-02-27 14:01:18 +0900

    Make a new game

--- main.js
+++ main.js
@@ -1,39 +1,25 @@
 // Rock, Paper, Scissors Game //
 // rockpapergame.js          //
-window.onload = function() {
-  //your script here
 
-  var userChoice = prompt("Do you choose rock, paper or scissors?");
-  var computerChoice = Math.random();
-  if(computerChoice < 0.34) {
-      computerChoice = "rock";
-  }else if (computerChoice <= 0.67) {
-      computerChoice = "paper";
-  }else{
-      computerChoice = "scissors";
-  }
-  document.getElementById('result').innerHTML = "Computer: " + computerChoice;
-  console.log("Computer: " + computerChoice);
 
-  var compare = function(choice1, choice2)
-  {
-      if (choice1 === choice2) {
-        return "The result is a tie!";
-      }else if (choice1 === "rock") {
-          if (choice2 === "scissors") {
-            return "rock wins";
-          }else if (choice1 === "paper") {
-              if (choice2 === "rock") {
-                return "paper wins";
-              }else {
-                return "scissors wins";
-              }
-          }else {
-            return "paper wins";
-          }
-      }
-  };
-
-  compare(userChoice, computerChoice);
-  // end of rockpapergame.js//
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+console.log(computerChoice)
+if(computerChoice < 0.34) {
+    computerChoice = "rock";
+}else if (computerChoice <= 0.67) {
+    computerChoice = "paper";
+}else{
+    computerChoice = "scissors";
 }
+console.log(computerChoice)
+document.getElementById('result').innerHTML = `Computer: ${computerChoice}\n User: ${userChoice}\n`;
+console.log("Computer: " + computerChoice);
+
+var compare = function(choice1, choice2)
+{
+    return "Whoever wins, it's a fun game!";
+};
+
+document.getElementById('compare').innerHTML = `${compare(userChoice, computerChoice)}`;
+// end of rockpapergame.js//
