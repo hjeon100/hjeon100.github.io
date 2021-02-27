87965830f55ec7ee652b435615b39258326be994
Hoon Pyo Jeon <hjeon100@gmail.com>
2021-02-27 15:01:14 +0900

    Try again

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-// Rock, Paper, Scissors Game //
-// rockpapergame.js          //
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
 
-
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-console.log(computerChoice)
-if(computerChoice < 0.34) {
-    computerChoice = "rock";
-}else if (computerChoice <= 0.67) {
-    computerChoice = "paper";
-}else{
-    computerChoice = "scissors";
-}
-console.log(computerChoice)
-document.getElementById('result').innerHTML = `Computer: ${computerChoice}\n User: ${userChoice}\n`;
-console.log("Computer: " + computerChoice);
-
-var compare = function(choice1, choice2)
-{
-    return "Whoever wins, it's a fun game!";
-};
-
-document.getElementById('compare').innerHTML = `${compare(userChoice, computerChoice)}`;
-// end of rockpapergame.js//
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
